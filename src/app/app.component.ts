959ac0578e8b1d4a63a61019e52e4839efcd5840
Showkat Akbar mithu <showkat131080@gmail.com>
2024-11-06 00:06:48 +0600

    font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+// import pdfMake from "pdfmake/build/pdfmake";
+import  pdfMake from 'pdfmake/build/pdfmake';
+//import  pdfFonts from 'pdfmake/build/vfs_fonts';
+
+// You need to assign pdfFonts to pdfMake to make the fonts available
+//(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
+
+pdfMake.fonts = {
+  Roboto: {
+    normal: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf',
+    bold: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf',
+    italics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf',
+    bolditalics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf',
+  },
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'FontDemo';
+
+  generatePdf(){
+    let docDifi={
+      content:['ভাষা শনাক্ত করুন']
+    };
+
+    pdfMake.createPdf(docDifi).open();
+  }
 }
